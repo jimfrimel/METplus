62455b0d88dcad6cc98d8d1772dc4de05ec0ad7e
Jim Frimel <james.t.frimel@noaa.gov>
2020-04-21 21:01:23 -0600

    Removed dev and v3.0 to replace with actual dev and v3.0

--- dev/doxygen/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['emc_2dncep_2dnoaa_20produtil_20library_20utility_20package',['EMC-NCEP-NOAA produtil library utility package',['../METplus_produtil.html',1,'']]]
-];
--- v3.0/doxygen/search/classes_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hangupsignal',['HangupSignal',['../classprodutil_1_1sigsafety_1_1HangupSignal.html',1,'produtil::sigsafety']]]
-];
